569d70c9d9af57989bde8a27056b3b75aa20220b
Annette Brookman <abrookm@l-sb9g2glg3q-m.local>
2016-05-12 12:52:33 -0700

    Initial Commit

---
+++ AB_Tipping/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AB_Tipping
+//
+//  Created by Annette Brookman on 5/12/16.
+//  Copyright © 2016 Annette Brookman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ AB_TippingTests/AB_TippingTests.swift
@@ -0,0 +1,36 @@
+//
+//  AB_TippingTests.swift
+//  AB_TippingTests
+//
+//  Created by Annette Brookman on 5/12/16.
+//  Copyright © 2016 Annette Brookman. All rights reserved.
+//
+
+import XCTest
+@testable import AB_Tipping
+
+class AB_TippingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
